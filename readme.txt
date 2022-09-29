8ecdb2f7c5409c98b691800cbf750a64c89f04db
UrielWo <96925853+UrielWo@users.noreply.github.com>
2022-09-30 00:27:59 +0300

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+automation for guest to join webex meetings.
+
+first you need to copy and run the mouse.bat on the C:\Windows dir, and then run the batch with the Task Scheduler.
---
+++ webex_meeting_batch.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+START https://XXXXXX.webex.com/meet/XXXXXXXXXXX
+
+timeout /t 10
+
+CALL  mouse moveTo 600x460
+
+CALL  mouse click
+
+timeout /t 10
+
+CALL  mouse moveTo 780x620
+
+CALL  mouse click
